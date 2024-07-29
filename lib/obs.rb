2c5d017c16089f077ab328852cd5ba2989ed93e9
Dani Donisa <dani.doni@gmail.com>
2024-07-29 15:17:39 +0200

    Update the monitoring configuration for 15.6
    Due to the recent upgrade of our reference instance server on
    https://build.opensuse.org to 15.6, we need to sync the monitoring configuration.

--- lib/obs.rb
+++ lib/obs.rb
@@ -27,7 +27,7 @@ class Obs
     @trello.status = status
     @trello.notify
 
-    slack_message = 'Build failed for obs-server. https://build.opensuse.org/package/live_build_log/OBS:Server:Unstable/obs-server/15.5/x86_64'
+    slack_message = 'Build failed for obs-server. https://build.opensuse.org/package/live_build_log/OBS:Server:Unstable/obs-server/15.6/x86_64'
     @slack.notify(message: slack_message) if status == :failed
   end
 
